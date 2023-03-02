2f54bab72492cfdb40928d4e7e5fa3ac2d4b0bda
Jeckson Sampayo <jsmsolucionesvalledupar@gmail.com>
2023-03-02 17:23:51 -0500

    documentando controladores

--- controllers/productController.php
+++ controllers/productController.php
@@ -3,14 +3,28 @@ include("./models/database.php");
 
 class ProductController
 {
+    /**
+     * Buscar los productos y nombre de la categoria a la cual pertenece
+     * 
+     * @return array listado de produtos
+     */
     public function getAllProducts()
     {
-        $sql = "SELECT *, (SELECT nombre FROM categorias WHERE categorias.id = productos.id_categoria limit 1) as categoria  FROM productos";
+        $sql = "SELECT *, 
+        (SELECT nombre FROM categorias WHERE categorias.id = productos.id_categoria limit 1) 
+        as categoria FROM productos";
 
         $products = Connection::getCustom($sql);
         return $products;
     }
 
+    /**
+     * Busca un producto especifico
+     * 
+     * @param int $id numero de identificador del producto
+     * 
+     * @return array producto seleccionado
+     */
     public function getOneProdut($id)
     {
         $product = Connection::getOneData('productos', $id);
--- controllers/userController.php
+++ controllers/userController.php
@@ -2,6 +2,12 @@
 include("./models/database.php");
 
 class UserController{
+    /**
+     * Cambia la contraseña de un usuario
+     * 
+     * @param string $newPassword nueva contraseña
+     * 
+     */
     public function changePassword($newPassword) {
         $hash = password_hash($newPassword, PASSWORD_DEFAULT);
         $email = $_SESSION['usuario'];
